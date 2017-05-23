9ed05cf518bd60478dce3ada1567c9f9a3f9f176
Kathleen P Chase <powerskem@gmail.com>
2017-05-23 17:19:17 -0400

    fixed cannot find symbol errorr

--- src/test/java/JavaTests.java
+++ src/test/java/JavaTests.java
@@ -1,13 +1,29 @@
-import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.After;
+
+import java.io.PrintStream;
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.Assert.assertEquals;
 import java.lang.*;
 
+import org.apache.http.*;
+
 public class JavaTests {
+
+    private final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outStream));
+    }
 
     @Test
     public void checkEnvVars() {
         // Check the currently set env vars against the preferred ones in pom.xml
         // These should be set in .hadooprc and sourced in ~hduser/.bashrc
+
         assertEquals("HADOOP_HOME not set",
             System.getenv("pref_HADOOP_HOME"),
             System.getenv("HADOOP_HOME"));
@@ -29,6 +45,16 @@ public class JavaTests {
         assertEquals("HADOOP_MAPRED_HOME not set",
             System.getenv("pref_HADOOP_MAPRED_HOME"),
             System.getenv("HADOOP_MAPRED_HOME"));
+    }
+
+    @Test
+    public void checkWebServers() {
+        // Check the nodemanager and resourcemanager web services
+    }
+
+    @After
+    public void cleanUp() {
+        System.setOut(null);
     }
 
 }
