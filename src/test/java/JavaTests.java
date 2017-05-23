7ddce379679ba871d360399c183031300211b61f
Kathleen P Chase <powerskem@gmail.com>
2017-05-23 16:10:56 -0400

    added checkEnvVars() to JavaTests to check env vars with pref_* in pom.xml

--- src/test/java/JavaTests.java
+++ src/test/java/JavaTests.java
@@ -5,13 +5,29 @@ import java.lang.*;
 public class JavaTests {
 
     @Test
-    public void check_assertEquals() {
-        assertEquals("TESTING_HOME not set",6,6);
-    }
-
-    @Test
-    public void check_getEnv() {
-        assertEquals("HADOOP_CONF_DIR not set","/home/hduser/hadoop/conf",System.getenv("HADOOP_CONF_DIR"));
+    public void checkEnvVars() {
+        // Check the currently set env vars against the preferred ones in pom.xml
+        assertEquals("HADOOP_HOME not set",
+            System.getenv("pref_HADOOP_HOME"),
+            System.getenv("HADOOP_HOME"));
+        assertEquals("YARN_CONF_DIR not set",
+            System.getenv("pref_YARN_CONF_DIR"),
+            System.getenv("YARN_CONF_DIR"));
+        assertEquals("HADOOP_CONF_DIR not set",
+            System.getenv("pref_HADOOP_CONF_DIR"),
+            System.getenv("HADOOP_CONF_DIR"));
+        assertEquals("HADOOP_COMMON_HOME not set",
+            System.getenv("pref_HADOOP_COMMON_HOME"),
+            System.getenv("HADOOP_COMMON_HOME"));
+        assertEquals("HADOOP_HDFS_HOME not set",
+            System.getenv("pref_HADOOP_HDFS_HOME"),
+            System.getenv("HADOOP_HDFS_HOME"));
+        assertEquals("HADOOP_YARN_HOME not set",
+            System.getenv("pref_HADOOP_YARN_HOME"),
+            System.getenv("HADOOP_YARN_HOME"));
+        assertEquals("HADOOP_MAPRED_HOME not set",
+            System.getenv("pref_HADOOP_MAPRED_HOME"),
+            System.getenv("HADOOP_MAPRED_HOME"));
     }
 
 }
