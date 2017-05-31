b17d6328ec62f7446facc85e46d4c4c1c67f3034
Kathleen P Chase <powerskem@gmail.com>
2017-05-31 18:00:11 -0400

    added tests

--- src/test/bin/hadoop-startup-tests.sh
+++ src/test/bin/hadoop-startup-tests.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+trap "exit 0" TERM
+top_pid=$$
 
-testStartAll()
-{
+oneTimeSetUp() {
+    input_file="Civil.txt"
+    mydate="$(date +%Y-%m-%d_%H%M%S)"
+    output_folder="testout_${mydate}"
+
+    echo "PID is $top_pid"
     echo "starting processes"
 
     hadoop-daemon.sh start namenode
@@ -12,8 +18,7 @@ testStartAll()
     mr-jobhistory-daemon.sh start historyserver
 }
 
-testProcessesRunning()
-{
+testProcessesRunningBefore() {
     for processtotest in \
         $(echo -e "NameNode \nDataNode \nSecondaryNameNode \nResourceManager \nNodeManager \nJobHistoryServer")
     do
@@ -23,15 +28,53 @@ testProcessesRunning()
     done
 }
 
-testExample1()
-{
-    #hdfs dfs -rm -r books/out
-    hadoop jar /usr/hdeco/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
-        grep books/Civilisation.txt books/out2 'my[a-z.]+'
+testFsck() {
+    assertEquals "File system / is not healthy" \
+        "Status: HEALTHY" \
+        "$(hdfs fsck / 2>&1 | grep -o --max-count=1 'Status: HEALTHY' )"
 }
 
-testStopAll()
-{
+testInputExists() {
+    assertNotEquals "Input file ${input_file} doesn't exist" \
+        "No such file or dir" \
+        "$(hdfs dfs -ls 2>&1 | grep -o --max-count=1 'No such file or dir')"
+}
+
+testOutputFolderDoesntExist() {
+    assertEquals "Output folder ${output_folder} exists already" \
+        "No such file or dir" \
+        "$(hdfs dfs -ls ${output_folder} 2>&1 | grep -o --max-count=1 'No such file or dir' )"
+}
+
+testMapredGrep() {
+    hadoop jar /usr/hdeco/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
+        grep ${input_file} ${output_folder} 'my[a-z.]+' 2> /tmp/hout_${mydate} & testpid=$!
+    wait $testpid
+    for job in \
+        $(grep "Running job:" /tmp/hout_${mydate} |cut -d' ' -f7)
+    do
+        assertEquals "Job $job was not completed successfully" \
+            "successfully" \
+            "$(grep $job /tmp/hout_${mydate} | grep completed | grep -o successfully)"
+    done
+    assertEquals "Mapreduce grep example did not produce correct result." \
+        "11" \
+        "$(hdfs dfs -cat ${output_folder}/part-r-00000 2>&1 | grep mythological | grep -o 11)"
+}
+
+testProcessesRunningAfter() {
+    for processtotest in \
+        $(echo -e "NameNode \nDataNode \nSecondaryNameNode \nResourceManager \nNodeManager \nJobHistoryServer")
+    do
+        assertEquals "$processtotest is no longer running (after runs)" \
+            "${processtotest}" \
+            "$(jps | cut -d' ' -f 2 | grep -o --max-count=1 $processtotest )"
+    done
+}
+
+testStopAll() {
+    hdfs dfs -rm -r $output_folder
+    rm -rf /tmp/hout_${mydate}
     echo "stopping processes"
 
     mr-jobhistory-daemon.sh stop historyserver
@@ -48,6 +91,9 @@ testStopAll()
             "0" \
             "$(jps | cut -d' ' -f 2 | grep -c $processtotest )"
     done
+}
+oneTimeTearDown() {
+    echo done
 }
 
 . ../lib/shunit2-2.1.6/src/shunit2
