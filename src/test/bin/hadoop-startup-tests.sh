537208502c487f72c1cda5f3ef25e40ad9f1269f
Kathleen P Chase <powerskem@gmail.com>
2017-06-06 11:17:34 -0400

    added wordcount & pi tests & wait for NameNode leave safe

--- src/test/bin/hadoop-startup-tests.sh
+++ src/test/bin/hadoop-startup-tests.sh
@@ -10,7 +10,9 @@ oneTimeSetUp() {
     echo "PID is $top_pid"
     echo "starting processes"
 
-    hadoop-daemon.sh start namenode
+    hadoop-daemon.sh start namenode 
+    # wait for 9 seconds for name node to automatically leave safe mode
+    sleep 35 & nnwaitpid=$!
     hadoop-daemon.sh start datanode
     hadoop-daemon.sh start secondarynamenode
     yarn-daemon.sh start resourcemanager
@@ -19,12 +21,14 @@ oneTimeSetUp() {
 }
 
 testProcessesRunningBefore() {
+    assertEquals "NameNode is not running" \
+        ' NameNode' \
+        "$(jps | grep -o -m1 ' NameNode' )"
     for processtotest in \
-        $(echo -e "NameNode \nDataNode \nSecondaryNameNode \nResourceManager \nNodeManager \nJobHistoryServer")
-    do
+      DataNode SecondaryNameNode ResourceManager NodeManager JobHistoryServer ; do
         assertEquals "$processtotest is not running" \
             "${processtotest}" \
-            "$(jps | cut -d' ' -f 2 | grep -o --max-count=1 $processtotest )"
+            "$(jps | grep -o -m1 $processtotest )"
     done
 }
 
@@ -47,7 +51,10 @@ testOutputFolderDoesntExist() {
 }
 
 testMapredGrep() {
-    hadoop jar /usr/hdeco/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
+    echo "Waiting for NameNode to leave safe mode"
+    wait $nnwaitpid ; # wait for name node to leave safe mode
+    echo "Processing hadoop job..."
+    hadoop jar ${HADOOP_MAPRED_HOME}/hadoop-mapreduce-examples.jar \
         grep ${input_file} ${output_folder} 'my[a-z.]+' 2> /tmp/hout_${mydate} & testpid=$!
     wait $testpid
     for job in \
@@ -59,24 +66,64 @@ testMapredGrep() {
     done
     assertEquals "Mapreduce grep example did not produce correct result." \
         "11" \
-        "$(hdfs dfs -cat ${output_folder}/part-r-00000 2>&1 | grep mythological | grep -o 11)"
+        "$(hdfs dfs -cat ${output_folder}/part-r-00000 2>&1 | \
+            grep mythological -m1 | cut -f1)"
+    hdfs dfs -rm -r $output_folder
+}
+
+testMapredWordcount() {
+    echo "Waiting for NameNode to leave safe mode"
+    wait $nnwaitpid ; # wait for name node to leave safe mode
+    echo "Processing hadoop job..."
+    hadoop jar ${HADOOP_MAPRED_HOME}/hadoop-mapreduce-examples.jar \
+        wordcount $input_file $output_folder 2>> /tmp/hout_${mydate} & testpid=$!
+    wait $testpid
+    for job in \
+        $(grep "Running job:" /tmp/hout_${mydate} |cut -d' ' -f7)
+    do
+        assertEquals "Job $job was not completed successfully" \
+            "successfully" \
+            "$(grep $job /tmp/hout_${mydate} | grep completed | grep -o successfully)"
+    done
+    assertEquals "Mapreduce wordcount example did not produce correct result." \
+        "1" \
+        "$(hdfs dfs -cat ${output_folder}/part-r-00000 2>&1 | \
+            grep wedding-feast | cut -f2)"
+    hdfs dfs -rm -r $output_folder
+}
+
+testMapredPi() {
+    echo "Waiting for NameNode to leave safe mode"
+    wait $nnwaitpid ; # wait for name node to leave safe mode
+    echo "Processing hadoop job..."
+    hadoop jar ${HADOOP_MAPRED_HOME}/hadoop-mapreduce-examples.jar \
+        pi 2 2 2>> /tmp/hout_${mydate} & testpid=$!
+    wait $testpid
+    for job in \
+        $(grep "Running job:" /tmp/hout_${mydate} |cut -d' ' -f7)
+    do
+        assertEquals "Job $job was not completed successfully" \
+            "successfully" \
+            "$(grep $job /tmp/hout_${mydate} | grep completed | grep -o successfully)"
+    done
 }
 
 testProcessesRunningAfter() {
+    assertEquals "NameNode is not running" \
+        ' NameNode' \
+        "$(jps | grep -o -m1 ' NameNode' )"
     for processtotest in \
-        $(echo -e "NameNode \nDataNode \nSecondaryNameNode \nResourceManager \nNodeManager \nJobHistoryServer")
-    do
+      DataNode SecondaryNameNode ResourceManager NodeManager JobHistoryServer ; do
         assertEquals "$processtotest is no longer running (after runs)" \
             "${processtotest}" \
-            "$(jps | cut -d' ' -f 2 | grep -o --max-count=1 $processtotest )"
+            "$(jps | grep -o -m1 $processtotest )"
     done
 }
 
 testStopAll() {
-    hdfs dfs -rm -r $output_folder
-    rm -rf /tmp/hout_${mydate}
-    echo "stopping processes"
+   #rm -rf /tmp/hout_${mydate}
 
+    echo "stopping processes"
     mr-jobhistory-daemon.sh stop historyserver
     yarn-daemon.sh stop nodemanager
     yarn-daemon.sh stop resourcemanager
@@ -85,11 +132,10 @@ testStopAll() {
     hadoop-daemon.sh stop namenode
 
     for processtotest in \
-        $(echo -e "NameNode \nDataNode \nSecondaryNameNode \nResourceManager \nNodeManager \nJobHistoryServer")
-    do
+      " NameNode" DataNode SecondaryNameNode ResourceManager NodeManager JobHistoryServer ; do
         assertEquals "$processtotest is still running" \
             "0" \
-            "$(jps | cut -d' ' -f 2 | grep -c $processtotest )"
+            "$(jps | grep -c $processtotest )"
     done
 }
 oneTimeTearDown() {
