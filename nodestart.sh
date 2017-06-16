0e17c414c5f41c0a2a8e0f367146dd7daecd450c
Kathleen P Chase <powerskem@gmail.com>
2017-06-16 12:22:49 -0400

    removed backgrounding on start/stop daemons

--- nodestart.sh
+++ nodestart.sh
@@ -6,14 +6,14 @@ if ps -p $nn_pid > /dev/null ; then
     exit 1
 fi
 
-hadoop-daemon.sh start datanode & dn_pid=$! ; wait $dn_pid
+#hadoop-daemon.sh start datanode
 ssh node2 "hadoop-daemon.sh start datanode"
 ssh node3 "hadoop-daemon.sh start datanode"
 #ssh node4 "hadoop-daemon.sh start datanode"
 #ssh node5 "hadoop-daemon.sh start datanode"
 
-yarn-daemon.sh start resourcemanager & rm_pid=$! ; wait $rm_pid
-yarn-daemon.sh start nodemanager & rm_pid=$! ; wait $rm_pid
+yarn-daemon.sh start resourcemanager
+yarn-daemon.sh start nodemanager
 
 #yarn-daemon.sh start proxyserver
 
--- nodestop.sh
+++ nodestop.sh
@@ -3,13 +3,13 @@
 mr-jobhistory-daemon.sh stop historyserver
 #yarn-daemon.sh stop proxyserver
 yarn-daemon.sh stop nodemanager
-yarn-daemon.sh stop resourcemanager & rm_pid=$! ; wait $rm_pid
+yarn-daemon.sh stop resourcemanager
 
-hadoop-daemon.sh stop datanode & dn_pid=$! ; wait $dn_pid
-ssh node2 "hadoop-daemon.sh stop datanode" & nn2_pid=$!
-ssh node3 "hadoop-daemon.sh stop datanode" & nn3_pid=$!
-#ssh node4 "hadoop-daemon.sh stop datanode" & nn4_pid=$!
-#ssh node5 "hadoop-daemon.sh stop datanode" & nn5_pid=$!
+hadoop-daemon.sh stop datanode
+ssh node2 "hadoop-daemon.sh stop datanode"
+ssh node3 "hadoop-daemon.sh stop datanode"
+#ssh node4 "hadoop-daemon.sh stop datanode"
+#ssh node5 "hadoop-daemon.sh stop datanode"
 
-hadoop-daemon.sh stop namenode & nn_pid=$! ; wait $nn_pid
+hadoop-daemon.sh stop namenode
 
